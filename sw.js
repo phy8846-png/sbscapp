138c0ee2106ed752466db92c8bd64675dacdf4b0
phy8846-png <phy8846@gmail.com>
2026-08-22 22:39:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,26 @@
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBo9cW3ZVO6dyYTJM-zYPQLUkdL6StYvCw",
+  authDomain: "sbsbapp-6ea34.firebaseapp.com",
+  projectId: "sbsbapp-6ea34",
+  storageBucket: "sbsbapp-6ea34.firebasestorage.app",
+  messagingSenderId: "176953247293",
+  appId: "1:176953247293:web:fb3943c34e8b988cbc8c54",
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const title = (payload.notification && payload.notification.title) || "새 업무 배정";
+  const body = (payload.notification && payload.notification.body) || "";
+  self.registration.showNotification(title, {
+    body,
+    icon: "./icon-192.png",
+  });
+});
+
 self.addEventListener('install', (e) => self.skipWaiting());
 self.addEventListener('activate', (e) => self.clients.claim());
 self.addEventListener('fetch', (e) => {
